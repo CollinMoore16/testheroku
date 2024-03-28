73eababe77875146db4ca1a717d75bbc98fc7730
CollinMoore16 <ccmoore@pointpark.edu>
2024-03-28 14:00:10 -0400

    Page layout with homepage picture working

--- index.go
+++ index.go
@@ -14,18 +14,18 @@ func main() {
 				<title> My Website!</title>
 			</head>	
 			<body>
-				<h1> Welcome to Yizer Country</h1>
-				<p>Fun Fact! Did you know that we have 105 microbreweries?</p>
-				<img src = "https://www.lonelyplanet.com/usa/pennsylvania/pittsburgh", height="250", width="250"
+				<h1> Welcome to My website!</h1>
+				<p>My name is Collin and i am currently a Junior at Point Park University!</p>
+				<img src = "https://yt3.googleusercontent.com/ytc/AIdro_k2JN_PlW7iDX6Dm87p8cmF1PnnDtBY3wT6g9UvNQ=s900-c-k-c0x00ffffff-no-rj", height="250", width="250"
 			</body>
 		</html>
 		
 		`)
 	})
 
-	//Sports Page
+	//Hobby Page
 
-	http.HandleFunc("/sports", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/hobbies", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, `
 		<html>
 			<head>
@@ -34,15 +34,16 @@ func main() {
 			<body>
 				<h1> Welcome to the Steelers Country</h1>
 				<p>Kordell Stewart Days are back again OH YEA</p>
-				<img src = "https://www.lonelyplanet.com/usa/pennsylvania/pittsburgh", height="250", width="250"
+				<img src = "PointParkPioneers.png", height="250", width="250"
 			</body>
 		</html>
 		
 		`)
 	})
-	//Hobby page
 
-	http.HandleFunc("/hobbies", func(w http.ResponseWriter, r *http.Request) {
+	//About page
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, `
 		<html>
 			<head>
